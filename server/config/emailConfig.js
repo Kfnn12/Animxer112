83cf407c6f4128f4cba15092965fdc4c1973a17e
DESKTOP-09UFEVH\kaker <taherdodiya87@gmail.com>
2023-04-16 22:54:52 +0530

    email template code and password changes still remains pending

--- server/config/emailConfig.js
+++ server/config/emailConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   port: 587,
   secure: false,
   auth: {
-    user: 'textify80@gmail.com',
+    user: 'animetrix013@gmail.com',
     pass: process.env.EMAIL_PASSWORD || "nacatwkwalqpnbzk",
   },
 };
