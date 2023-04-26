30f627bc26584538917fb8f34f480d772fe9cecd
Shiva <shivaxwarrior@gmail.com>
2023-04-26 22:32:22 +0530

    changed welcome greeting email

--- server/utils/email.js
+++ server/utils/email.js
@@ -13,14 +13,13 @@ async function sendVerificationEmail(to, token) {
             <html lang="en">
             <head>
               <meta charset="UTF-8">
-              <title>Verification OTP for AnimeTrix</title>
+              <title>Login code for AnimeTrix</title>
             </head>
             <body>
               <h1>Hello!</h1>
-              <p>Thank you for signing up for our AnimeTrix. As part of our verification process, please use the following OTP code:</p>
-              <h2>OTP Code: ${token}</h2>
-              <p>Please enter this code on our website to verify your email address and activate your account.</p>
-              <p>If you did not sign up for our website, please ignore this email.</p>
+              <p>Thank you for choosing AnimeTrix as your go-to platform for watching anime. As a registered user, we're providing you with a special login code to access our additional features</p>
+              <h2>Your login code is:  ${token}</h2>
+              <p>To get started, simply enter this code when prompted upon logging in to your account.</p>
               <p>Thank you,<br>AnimeTrix Team</p>
             </body>
             </html>`
