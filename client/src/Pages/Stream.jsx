a07ec0d27ac821f266feacf31434cc74ff5d8d4b
Shiva <shivaxwarrior@gmail.com>
2023-05-04 18:44:22 +0530

    Removed idk what hope the code works

--- client/src/Pages/Stream.jsx
+++ client/src/Pages/Stream.jsx
@@ -137,22 +137,7 @@ export default function Stream(props) {
       console.log("Error loading streaming data");
     }
   }
-
-  function playM3u8(video, url, art) {
-    if (Hls.isSupported()) {
-      if (art.hls) art.hls.destroy();
-      const hls = new Hls();
-      hls.loadSource(url);
-      hls.attachMedia(video);
-      art.hls = hls;
-      art.on('destroy', () => hls.destroy());
-    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-      video.src = url;
-    } else {
-      art.notice.show = 'Unsupported playback format: m3u8';
-    }
-  }
-
+  
   const getDetails = async () => {
     try {
       const api = await fetch(`${HomeApi}/meta/anilist/info/${animeId}`)
